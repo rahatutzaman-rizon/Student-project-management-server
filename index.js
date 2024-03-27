943df88a0e4f74a9f73799728818c3d887bc83a0
Rahatutzaman Rizon <115326466+rahatutzaman-rizon@users.noreply.github.com>
2024-03-27 17:25:00 +0600

    add new

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const port = process.env.PORT | 5000;
 app.use(express.json());
 app.use(cors());
 
-//xXVjxb8oqcLnNrs0
+//gpeYJ3jTyAALnHAr
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = "mongodb://rizonrahat199:xXVjxb8oqcLnNrs0@ac-jif2aos-shard-00-00.u9sh80h.mongodb.net:27017,ac-jif2aos-shard-00-01.u9sh80h.mongodb.net:27017,ac-jif2aos-shard-00-02.u9sh80h.mongodb.net:27017/?ssl=true&replicaSet=atlas-rzyffr-shard-0&authSource=admin&retryWrites=true&w=majority";
+const uri = "mongodb://rizonrahat199:gpeYJ3jTyAALnHAr@ac-jif2aos-shard-00-00.u9sh80h.mongodb.net:27017,ac-jif2aos-shard-00-01.u9sh80h.mongodb.net:27017,ac-jif2aos-shard-00-02.u9sh80h.mongodb.net:27017/?ssl=true&replicaSet=atlas-rzyffr-shard-0&authSource=admin&retryWrites=true&w=majority";
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -22,14 +22,85 @@ async function run() {
   try {
     const taskCollection = client.db("jobTask").collection("tasks");
     const groupCollection= client.db("group").collection("member");
-    // groupCollection.insertOne({
-    //   "name":"purno",
-    //   "it":"19031",
-    //   "group":"1",
-    //   "category":"student",
+    const teacherCollection= client.db("teacher").collection("one");
+    const teacherCollection2= client.db("teacher").collection("two");
 
+
+    app.get("/1", async (req, res) => {
+      const result = await teacherCollection.find().toArray();
+      res.send(result);
+    })
+
+    app.get("/2", async (req, res) => {
+      const result = await teacherCollection2.find().toArray();
+      res.send(result);
+    })
+
+
+    app.get('/team1/:id',async(req,res)=>{
+      const id=req.params.id;
+      const query={_id:new ObjectId(id)};
+      const mover=await teacherCollection.findOne(query);
+      
+      res.send(mover);
+     
+    })
+    
+    app.get('/team2/:id',async(req,res)=>{
+      const id2=req.params.id;
+      const query2={_id:new ObjectId(id2)};
+      const mover2=await teacherCollection2.findOne(query2);
+      res.send(mover2);
+   })
+    
+    
+
+
+
+
+//put method 
+app.put("/taskUpdate/:id",async(req,res)=>{
+
+  const tasks=req.body;
+  const id = req.params.id;
+
+  console.log(tasks);
+  const result=await teacherCollection.findOneAndUpdate(
+    
+      
+{ team: id },
+{ $push: { tasks: tasks} },
+    
+  );
+  res.send(result)
+
+})
+
+
+
+    // app.get('/1/:id',async(req,res)=>{
+    //   const id=req.params.id;
+    //   const query={_id:new ObjectId(id)};
+    //   const mover=await teacherCollection.findOne(query);
+    //   res.send(mover)
+    // })
+    
+
+    // app.post('/1/:id',async(req,res)=>{
+  
+    
+    //   const member=req.body;
+   
+      
+    //   const result=await teacherCollection.insertOne(member);
+    
+    //   res.send(result);
 
     // })
+    
+   
+    
+
 
     app.get("/getstudent",async(req,res)=>{
       const result = await groupCollection.find({group:"2"}).toArray();
@@ -55,15 +126,6 @@ async function run() {
 
 
 
-      // app.get("/member/:id", async (req, res) => {
-      //   const id=req.params.id;
-      //  const query={
-      //   _id : new ObjectId(id)
-      //  }
-      //   const result = await groupCollection.findOne(query) ;
-      //   res.send(result);
-      // });
-      
 
       app.get("/member/:groupId", async (req, res) => {
         const groupId = req.params.groupId;
@@ -86,17 +148,6 @@ app.get('/move/:id',async(req,res)=>{
 })
 
 
-
-    app.post("/createTask", async (req, res) => {
-      const taskData = req?.body;
-      const result = await taskCollection.insertOne(taskData);
-      res.send(result);
-    })
-
-    app.get("/previousTask", async (req, res) => {
-      const result = await taskCollection.find().toArray();
-      res.send(result);
-    })
 
     app.delete("/taskDelete/:id", async (req, res) => {
       const id = req?.params?.id;
